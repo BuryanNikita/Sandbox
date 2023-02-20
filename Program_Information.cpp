85b19281fba49b2045a82dd89da3dbaf44901751
Buryan Nikita <125834786+BuryanNikita@users.noreply.github.com>
2023-02-21 01:28:39 +0300

    Add files via upload

---
+++ Program_Information.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <curses.h>
+#include <iostream>
+#include <string>
+#include <string.h>
+using namespace std;
+
+class Information
+{
+string name;
+string surname;
+int age;
+int height;
+public:
+void input();
+void output();
+};
+
+void Information::input()
+{
+cout << "Задайте данные: " << endl;
+cout << "Имя, Фамилия, Возраст, Рост " << endl;
+cin >> name >> surname >> age >> height;
+}
+
+void Information::output()
+{
+cout << name << " " << surname << " " << age << " " << height << "."<< endl;
+}
+
+int main()
+{
+Information a1;
+a1.input();
+a1.output();
+Information a2;
+a2.input();
+a2.output();
+return 0;
+}
